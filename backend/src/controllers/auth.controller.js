5e6bfcf894eeecb5cf23d0077e82d525044a6c17
Yash-Rawat-IMA <yash97181004@gmail.com>
2026-09-06 18:33:08 +0530

    Added login and logout end points

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -54,9 +54,44 @@ export async function signup(req, res) {
 }
 
 export async function login(req, res) {
-    res.send("Login Route")
+    try {
+        const {email, password} = req.body;
+        // No need to add validators at the time of login
+        if(!email || !password){
+            return res.status(400).json({message: "All fields are required"})
+        }
+        
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(401).json({message:"Invalid email or password"});
+        }
+
+        const isPasswordCorrect = await user.matchPassword(password);
+
+        if(!isPasswordCorrect){
+            return res.status(401).json({message:"Invalid email or password"}); //sending invalid email or password creates the authentication more secure as user can't know the email is present in the database or not 
+        }
+
+        const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET_KEY, {
+            expiresIn: "7d"
+        })
+
+        res.cookie("jwt", token, {
+            maxAge: 7 * 24 * 60 * 60 *1000,
+            httpOnly: true, //prevent XSS attacks,
+            sameSite: true, //prevent CSRF attacks,
+            secure: process.env.NODE_ENV === "production"   //for https when on production
+        })
+
+        return res.status(200).json({success:true, user});
+
+    } catch (error) {
+        console.error(`Error in login controller: ${error}`);
+        res.status(500).json({message: "Internal Server Error"});
+    }
 }
 
 export function logout(req, res) {
-    res.send("Logout Route")
+    res.clearCookie("jwt")
+    res.status(200).json({success:true, message:"Logout successfull"});
 }
--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -67,6 +67,11 @@ userSchema.pre("save", async function (next) {
     }
 })
 
+userSchema.methods.matchPassword = async function(enteredPassword){
+    const isPasswordCorrect = await bcrypt.compare(enteredPassword, this.password)
+    return isPasswordCorrect;
+}
+
 // Create the model
 const User = mongoose.model("User", userSchema);
 
