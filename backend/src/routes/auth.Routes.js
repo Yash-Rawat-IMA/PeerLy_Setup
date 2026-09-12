6a51a9b49b5157b2bd309cb87cb44464e97ba0c4
Yash-Rawat-IMA <yash97181004@gmail.com>
2026-09-12 18:31:18 +0530

    feat: getting friends and their requests based on status

--- backend/src/routes/auth.Routes.js
+++ backend/src/routes/auth.Routes.js
@@ -19,5 +19,6 @@ router.get("/me", protectRoute, (req, res) => {
     res.status(200).json({success: true, user: req.user});
 })
 
+//more functionalities like resetPassword, welcomeEmail 
 
 export default router; 
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import {getRecommendedUser, getMyFriends} from "../controllers/user.controller.js";
+import { getRecommendedUser, getMyFriends, sendFriendRequest, acceptFriendRequest, getFriendRequests, getOutgoingRequests } from "../controllers/user.controller.js";
 
 const router = express.Router();
 // apply auth middleware to all routes
@@ -8,5 +8,15 @@ router.use(protectRoute);
 
 router.get("/", getRecommendedUser);
 router.get("/friends", getMyFriends);
+
+router.post("/friend-request/:id", sendFriendRequest);
+
+router.put("/friend-request/:id/accept", acceptFriendRequest);
+
+router.get("/friend-requests", getFriendRequests);
+
+router.get("/outgoing-friend-requests", getOutgoingRequests);
+
+//more functionalities like rejecting a requests
 
 export default router;
