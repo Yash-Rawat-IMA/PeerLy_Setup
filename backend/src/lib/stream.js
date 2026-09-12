3c11eba9144499985446f1b58e76680adc997743
Yash-Rawat-IMA <yash97181004@gmail.com>
2026-09-12 18:51:59 +0530

    feat: get stream token for chat room

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,15 +1,16 @@
-import {StreamChat} from "stream-chat";
+import { StreamChat } from "stream-chat";
 import "dotenv/config";
 
 const apiKey = process.env.STREAM_API_KEY;
 const apiSecret = process.env.STREAM_API_SECRET;
 
-if(!apiKey || !apiSecret){
+if (!apiKey || !apiSecret) {
     console.error("Stream API Key or Secret is missing");
 }
 
 const streamClient = StreamChat.getInstance(apiKey, apiSecret);
 
+//upsert the user in stream
 export const upsertStreamUser = async (userData) => {
     try {
         await streamClient.upsertUsers([userData]); //upsert-create or update the user
@@ -19,7 +20,12 @@ export const upsertStreamUser = async (userData) => {
     }
 }
 
-//todo: do it later
 export const generateStreamToken = async (userId) => {
-
+    try {
+        // ensure userId is a string
+        const userIdStr = userId.toString();
+        return streamClient.createToken(userIdStr);
+    } catch (error) {
+        console.log("Error generating stream token: ", error);
+    }
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.Routes.js";
 import userRoutes from "./routes/user.routes.js";
+import chatRoutes from "./routes/chat.Routes.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -18,6 +19,7 @@ app.use(cookieParser());
 //routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on the port: ${PORT}`);
