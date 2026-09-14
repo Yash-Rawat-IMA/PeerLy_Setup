959e1467836bde3a9dee9d1b12a5ded0767fc594
Yash-Rawat-IMA <yash97181004@gmail.com>
2026-09-14 16:48:16 +0530

    feat: add signup page with form validation and mutation handling

---
+++ frontend/src/lib/api.js
@@ -0,0 +1,5 @@
+import { axiosInstance } from "./axios.js"
+export const signup = async (signupData) => {
+    const response = await axiosInstance.post("/auth/signup", signupData);
+    return response.data;
+};
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,7 +1,184 @@
-import React from "react";
+import React, { useState } from "react";
+import { ShipWheelIcon } from "lucide-react";
+import { Link } from "react-router";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { signup } from "../lib/api.js";
 
 const SignUpPage = () => {
-  return <div data-theme="forest">SignUpPage</div>;
+  const [signupData, setSignupData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+
+  const queryClient = useQueryClient();
+
+  const {mutate: signupMutation, isPending, error} = useMutation({
+    mutationFn: signup,
+    onSuccess: () =>  queryClient.invalidateQueries({queryKey: ["authUser"]})
+  });
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    signupMutation(signupData);
+  };
+
+  return (
+    <div
+      className="h-screen flex items-center justify-center p-4 sm:p-6 md:p-8"
+      data-theme="forest"
+    >
+      <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
+        {/* Sign up form - left side */}
+
+        <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col">
+          {/* logo */}
+          <div className="mb-4 flex items-center justify-start gap-2">
+            <ShipWheelIcon className="size-9 text-primary" />
+            <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider">
+              PeerLy
+            </span>
+          </div>
+
+          {/* Error Message if Any */}
+          {error && (
+            <div className="alert alert-error mb-4">
+              <span>
+                {error?.response?.data?.message || "Something went wrong. Please try again."}
+              </span>
+            </div>
+          )}
+
+          <div className="w-full">
+            <form onSubmit={handleSignup}>
+              <div className="space-y-4">
+                <div>
+                  <h2 className="text-xl font-semibold">Create an Account</h2>
+                  <p className="text-sm opacity-70">
+                    Join PeerLy and Start your peer learning adventure!
+                  </p>
+                </div>
+
+                <div className="space-y-3">
+                  {/* Full Name */}
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text">Full Name</span>
+                    </label>
+
+                    <input
+                      type="text"
+                      placeholder="John Doe"
+                      className="input input-bordered w-full"
+                      value={signupData.fullName}
+                      onChange={(e) =>
+                        setSignupData({
+                          ...signupData,
+                          fullName: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                  </div>
+
+                  {/* Email */}
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text">Email</span>
+                    </label>
+
+                    <input
+                      type="email"
+                      placeholder="john@gmail.com"
+                      className="input input-bordered w-full"
+                      value={signupData.email}
+                      onChange={(e) =>
+                        setSignupData({ ...signupData, email: e.target.value })
+                      }
+                      required
+                    />
+                  </div>
+
+                  {/* Password */}
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text">Password</span>
+                    </label>
+
+                    <input
+                      type="password"
+                      placeholder="********"
+                      className="input input-bordered w-full"
+                      value={signupData.password}
+                      onChange={(e) =>
+                        setSignupData({
+                          ...signupData,
+                          password: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                    <p className="text-xs opacity-70 mt-1">
+                      Password must be atleast 6 characters
+                    </p>
+                  </div>
+
+                  {/* Terms and Conditions Checkbox */}
+                  <div className="form-control">
+                    <label className="label cursor-pointer justify-start gap-2">
+                      <input type="checkbox" className="checkbox checkbox-sm" required />
+                      <span className="text-xs leading-tight">
+                        I agree to the {" "}
+                        <span className="text-primary hover:underline">
+                          terms of service
+                        </span>and {" "}
+                        <span className="text-primary hover:underline">privacy policy</span>
+                      </span>
+                    </label>
+                  </div>
+                </div>
+
+              <button className="btn btn-primary w-full" type="submit">
+                {isPending ? (
+                  <>
+                  <span className="loading loading-spinner loading-xs">
+                  </span>
+                  Loading...
+                  </>
+                ) : (
+                  "Create Account"
+                )}
+                </button>
+
+              <div>
+                <p>Already have an Account?{" "}
+                  <Link to="/login" className="text-primary hover:underline">Sign In</Link>
+                </p>
+              </div>
+
+              </div>
+            </form>
+          </div>
+        </div>
+
+        {/* Sign up form - right side */}
+        <div className="hidden lg:flex w-full lg:w-1/2 bg-primary/10 items-center justify-center">
+          <div className="max-w-md p-8">
+            {/* Illustration */}
+            <div className="relative aspect-square max-w-sm mx-auto">
+              <img src="/SignUp_PeerLy.png" alt="Peer Learning Illustrations" />
+            </div>
+
+            <div className="text-center space-y-3 mt-6">
+              <h2 className="text-xl font-semibold">Connect with your Peer</h2>
+              <p className="opacity-70">Practice together, Make Friends and Improve your skills</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
 };
 
 export default SignUpPage;
