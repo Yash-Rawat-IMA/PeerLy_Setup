6cd8e8d84b849400acc327b9cc79d38d49304e91
Yash-Rawat-IMA <yash97181004@gmail.com>
2026-09-24 13:36:59 +0530

    chore: prepare PeerLy for production deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,16 +6,17 @@ import authRoutes from "./routes/auth.Routes.js";
 import userRoutes from "./routes/user.routes.js";
 import chatRoutes from "./routes/chat.Routes.js";
 import { connectDB } from "./lib/db.js";
+import path from "path";
 
 dotenv.config();
 // import "dotenv/config";
 
 const app = express();
 const PORT = process.env.PORT;
-
+const __dirname = path.resolve();
 // middleware
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin: "http://localhost:5173",
     credentials: true, // allow frontend to send the cookies 
 }))
 app.use(express.json());
@@ -25,6 +26,14 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
+
+if (process.env.NODE_ENV == "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on the port: ${PORT}`);
